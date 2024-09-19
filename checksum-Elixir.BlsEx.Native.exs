e7616da0d036957cd553978d14af247c35b2a339
Samuel Manzanera <samuelmanzanera@protonmail.com>
2024-09-19 16:55:20 +0200

    add checksum file

---
+++ checksum-Elixir.BlsEx.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "bls-v0.1.1-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:5e25961798f79720990a5a66aaadab44993e96003532ba0efa6d643021ddb6e9",
+  "bls-v0.1.1-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:bd9525bbf5bfd26ba730412b46c7a555d6e3f5083c8807d13f7d5aa6602dd9b5",
+  "libbls-v0.1.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:13c1594d35ffccf1361fb271f46b841c8015b9402325b851d4cb7cb50ed79739",
+  "libbls-v0.1.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:48f6ad02d1cb99de90e7785d289ca7ae6f98beb0d360d3247c52acabd24c3124",
+  "libbls-v0.1.1-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:30b261203b944330bc455b49a6bf1b53535a5bafde2c6a962e12eae951637d79",
+  "libbls-v0.1.1-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:a86df37ddc8aa3147100263213b49b9c004d73c1f7e56ebc91d2f2020bb71276",
+  "libbls-v0.1.1-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:6c05cd3ec27a783bb896f722d65addcba3eee419dfbef36f50274a3ff7c12128",
+  "libbls-v0.1.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:2badb1c62d51146928991ec1204fa633d92683b73073bea5228ba1e26f68e5ed",
+  "libbls-v0.1.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0d2f9db9cf1f5b8ab50dbcf2b9dc68a70316ec902e4a6375edbaf42e863a2e20",
+  "libbls-v0.1.1-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:74d811b0e052e821fe577b2a0e9ae61db07f0549edd36a442e3215e7ef4e1a68",
+}
