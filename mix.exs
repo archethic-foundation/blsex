0b27d9f6e7ede84136e810498c6a9ded7a1dd942
Neylix <julien.leclerc05@protonmail.com>
2024-11-15 09:51:13 +0100

    Up version 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BlsEx.MixProject do
   def project do
     [
       app: :bls_ex,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "BLS signature utility for Elixir",
